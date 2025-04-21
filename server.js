3c6ea38fba2d4c0142bf3ac455bf4ea4e6b21503
Ahmed Naeim <eng.a.naeim@gmail.com>
2025-04-21 18:31:12 +0200

    fix errors

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-const path = require('path');
 const express = require ('express');
 const app = express();
+const path = require('path');
 const cors = require('cors');
 const corsOptions = require('./config/corsOptions');
-
 
 const {logger} = require('./middleware/logEvents');
 const {errorHandler} = require('./middleware/errorHandler');
